8b88ecef84fba867500dae98db299a94d68e8186
acsdavid97 <acsdavid97@gmail.com>
2017-10-14 19:52:35 +0100

    FP second assignment in nearly working condition

--- FP/practice_lab/Lab-2/Main.hs
+++ FP/practice_lab/Lab-2/Main.hs
@@ -4,16 +4,21 @@
  - Assignment: 02.
  -}
 
-paint_interior_of_wall :: [a] -> [a] -> [a]
-paint_interior_of_wall _ [] = []
-paint_interior_of_wall _ [a] = [a]
-paint_interior_of_wall (colour:colours) (brick:bricks) =
-    colour : paint_interior_of_wall colours bricks
+paint_interior_of_wall :: [a] -> [a] -> [a] -> [a]
+paint_interior_of_wall _ _ [] = []
+paint_interior_of_wall _ _ [a] = [a]
+paint_interior_of_wall [] orignal_colours wall = 
+    paint_interior_of_wall orignal_colours orignal_colours wall
+paint_interior_of_wall (colour:colours) orignal_colours (brick:bricks) =
+    colour : paint_interior_of_wall colours orignal_colours bricks
 
 paint_except_first_brick :: [a] -> [a] -> [a]
 paint_except_first_brick colours (brick:bricks) = 
-    brick : paint_interior_of_wall colours bricks
+    brick : paint_interior_of_wall colours colours bricks
 
+-- paints the interior of each wall, which are supplied as a list
+-- in the second argument to the function, using the colours cyclically 
+-- given in the first argument of the function. 
 paint_interior_bricks :: [a] -> [[a]] -> [[a]]
 paint_interior_bricks colours [] = []
 paint_interior_bricks colours (wall:walls) = 
