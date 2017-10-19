1c3370f8d79b8306779a6747980877b91d5ad68c
Ács Dávid <acsdavid97@gmail.com>
2017-10-19 13:49:36 +0100

    paint without comments

--- FP/practice_lab/Lab-2/Main.hs
+++ FP/practice_lab/Lab-2/Main.hs
@@ -1,45 +1,47 @@
-20 {-
-  19  - Name: Acs David.
-  18  - Number: 117106523.
-  17  - Assignment: 02.
-  16  -}
-  15 
-  14 paint_wall :: [a] -> [a] -> [a] -> [a] -> [[a]] -> [[a]]
-  13 -- we ran out of colours
-  12 paint_wall painted_wall [] original_colours wall walls =
-  11     paint_wall painted_wall original_colours original_colours wall walls
-  10 -- at the end of the wall
-   9 paint_wall painted_wall colours original_colours [] walls =
-   8     paint_walls_recursively painted_wall colours original_colours walls
-   7 -- just a single brick
-   6 paint_wall painted_wall colours original_colours [brick] walls =
-   5     paint_wall (brick:painted_wall) colours original_colours [] walls
-   4 -- append colour to the beginning of the wall
-   3 paint_wall painted_wall (colour:colours) original_colours (brick:bricks) walls =
-   2     paint_wall (colour:painted_wall) colours original_colours bricks walls 
-   1 
- 21  paint_walls_recursively :: [a] -> [a] -> [a] -> [[a]] -> [[a]] 
-   1 paint_walls_recursively painted_wall _ _ [] = [painted_wall]
-   2 paint_walls_recursively painted_wall colours original_colours ((brick:bricks):walls) =
-   3     painted_wall : paint_wall [brick] colours original_colours bricks walls
-   4 
-   5 paint_interior_bricks :: [a] -> [[a]] -> [[a]]
-   6 paint_interior_bricks colours walls = paint_walls_recursively [] colours colours walls
-   7 
-   8 test1 :: IO ()
-   9 test1 = putStrLn (show (paint_interior_bricks colours walls))
-  10     where colours = [0, 1, 2]
-  11           walls = [[1, 2, 3 ,4 ,5 ,6 ,7 ,8, 9,0]]
-  12           
-  13 main :: IO ()
-  14 main = putStrLn (show (paint_interior_bricks colours walls))
-  15     where colours = [0, 1, 2]
-  16           walls = [[3],[3,3],[3,3,3],[3,3,3],[3,3,3,3,3],[3,3,3,3,3,3]]
- ~                                                                                                                  
- ~                                                                                                                  
- ~                                                                                                                  
- ~                                                                                                                  
- ~                                                                                                                  
- ~                                                                                                                  
- ~                                                                                                                  
- ~                     
+                                                                                      
+{-
+ - Name: Acs David.
+ - Number: 117106523.
+ - Assignment: 02.
+ -}
+ 
+paint_wall :: [a] -> [a] -> [a] -> [a] -> [[a]] -> [[a]]
+-- we ran out of colours
+paint_wall painted_wall [] original_colours wall walls =
+    paint_wall painted_wall original_colours original_colours wall walls
+-- at the end of the wall
+paint_wall painted_wall colours original_colours [] walls =
+    paint_wall_rev [] colours original_colours painted_wall walls
+-- just a single brick
+paint_wall painted_wall colours original_colours [brick] walls =
+    paint_wall (brick:painted_wall) colours original_colours [] walls
+-- append colour to the beginning of the wall
+paint_wall painted_wall (colour:colours) original_colours (brick:bricks) walls =
+    paint_wall (colour:painted_wall) colours original_colours bricks walls
+
+paint_wall_rev :: [a] -> [a] -> [a] -> [a] -> [[a]] -> [[a]]
+paint_wall_rev rev_wall colours original_colours [] walls =
+    paint_walls_recursively rev_wall colours original_colours walls
+paint_wall_rev rev_wall colours original_colours (brick:bricks) walls =
+    paint_wall_rev (brick:rev_wall) colours original_colours bricks walls
+
+paint_walls_recursively :: [a] -> [a] -> [a] -> [[a]] -> [[a]]
+paint_walls_recursively [] colours original_colours ((brick:bricks):walls) = 
+    paint_wall [brick] colours original_colours bricks walls
+paint_walls_recursively painted_wall _ _ [] = [painted_wall]
+paint_walls_recursively painted_wall colours original_colours ((brick:bricks):walls) =
+    painted_wall : paint_wall [brick] colours original_colours bricks walls
+    
+paint_interior_bricks :: [a] -> [[a]] -> [[a]]
+paint_interior_bricks colours walls = paint_walls_recursively [] colours colours walls
+ 
+test1 :: IO ()
+test1 = putStrLn (show (paint_interior_bricks colours walls))
+    where colours = [0, 1, 2]
+          walls = [[89, 2, 3 ,4 ,5 ,6 ,7 ,8, 9, 99]]
+
+main :: IO ()
+main = putStrLn (show (paint_interior_bricks colours walls))
+    where colours = [0, 1, 2]
+          walls = [[3],[3,3],[3,3,3], [3,3,3],[3,3,3,3,3],[3,3,3,3,3,3]]
+
