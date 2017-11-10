0e845b26b85fa6c493021b6b7a34f5a51b9d7b01
acsdavid97 <acsdavid97@gmail.com>
2017-11-10 16:52:22 +0000

    updated fourth fp assignment, to use the new constructor notation

--- FP/practice_lab/Lab-4/Main.hs
+++ FP/practice_lab/Lab-4/Main.hs
@@ -8,8 +8,12 @@ module Main where
 
 -- polymorphic, recursive data type List, 
 -- can be and EmptyList or an element and a List. 
--- Constructor is Cons
-data List a = EmptyList | Cons a (List a)
+-- Constructor is Cons. if the list is not empty it consists of 
+-- the first element and the rest of the elements.
+data List a = EmptyList |
+              Cons { first :: a
+                   , rest :: List a
+              }
 
 -- pretty prints a list, if the elements of the list are "showable"
 pretty_print :: (Show a) => List a -> String
@@ -24,8 +28,9 @@ pretty_print as = "[" ++ pretty_print_rec as
 -- comma is added to the string version of the list
 pretty_print_rec :: (Show a) => List a -> String
 pretty_print_rec EmptyList = "]" 
-pretty_print_rec (Cons a EmptyList) = show a ++ "]" 
-pretty_print_rec (Cons a as) = show a ++ "," ++ pretty_print_rec as
+pretty_print_rec (Cons {first=a, rest=EmptyList}) = show a ++ "]" 
+pretty_print_rec (Cons {first=a, rest=as}) = 
+            show a ++ "," ++ pretty_print_rec as
 
 -- Constructs an ordered List from a normal Haskell list([]).
 -- each of the elements is copied from the Haskell list and
@@ -36,21 +41,22 @@ create_from_list = foldr (flip insert) EmptyList
 -- insert an element in order, into an ordered list
 -- The function assumes that the list is ordered.
 insert :: (Ord a) => List a -> a -> List a
-insert EmptyList i = Cons i EmptyList
-insert l@(Cons a as) i | i <= a = Cons i l
-                       | otherwise = Cons a (insert as i)
+insert EmptyList i = Cons {first=i, rest=EmptyList}
+insert l@(Cons {first=a, rest=as}) i | i <= a = Cons {first=i, rest=l}
+                                     | otherwise = 
+                                         Cons {first=a, rest=(insert as i)}
 
 -- equivalent of foldl for Haskell lists.
 -- use an accumulator and forward recursion, apply i as first element
 foldl' :: (b -> a -> b) -> b -> List a -> b
 foldl' f i EmptyList = i
-foldl' f i (Cons a as) = foldl' f (f i a) as
+foldl' f i (Cons {first=a, rest=as}) = foldl' f (f i a) as
 
 -- equivalent of foldr for Haskell lists.
 -- backward recursion and apply i as last element
 foldr' :: (a -> b -> b) -> b -> List a -> b
 foldr' f i EmptyList = i
-foldr' f i (Cons a as) = f a (foldr' f i as)
+foldr' f i (Cons {first=a, rest=as}) = f a (foldr' f i as)
 
 -- tests create_from_list and pretty_print
 main :: IO()
